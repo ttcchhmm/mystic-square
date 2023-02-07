85b6c4b7d8792b1de1df80433c6fc8addd9ba631
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-02-07 12:09:40 +0100

    Implement localization support fr_FR and en_US supported

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <QApplication>
+#include <QTranslator>
+#include <QDebug>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    if(translator.load(QLocale(), "MysticSquare", "-")) {
+        qDebug() << "Loading translation from" << translator.filePath();
+        QCoreApplication::installTranslator(&translator);
+    }
 
     return QApplication::exec();
 }
