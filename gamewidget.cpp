b403fc7023d1dd8d36fd1dc88d30c4daf9e9225e
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-02-10 14:14:02 +0100

    Add the game widget

---
+++ gamewidget.cpp
@@ -0,0 +1,5 @@
+#include "gamewidget.hh"
+
+GameWidget::GameWidget(QWidget *parent): QWidget(parent) {
+
+}
---
+++ gamewidget.hh
@@ -0,0 +1,14 @@
+#ifndef MYSTICSQUARE_GAMEWIDGET_HH
+#define MYSTICSQUARE_GAMEWIDGET_HH
+
+#include <QWidget>
+
+class GameWidget: public QWidget {
+    Q_OBJECT
+
+public:
+    explicit GameWidget(QWidget* parent = nullptr);
+};
+
+
+#endif //MYSTICSQUARE_GAMEWIDGET_HH
