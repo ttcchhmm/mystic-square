2bbd00e6a32857e301bb8332e156966773901a63
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-02-20 19:05:53 +0100

    Prettify logs

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     QApplication::setWindowIcon(QIcon(":/assets/icon.png"));
 
+    qSetMessagePattern(QStringLiteral("[%{type} / %{file}:%{line} / %{function} / %{time yyyy-MM-dd h:mm:ss}]: %{message}"));
+
     QTranslator translator;
     if(translator.load(QLocale(), "MysticSquare", "-")) {
         qDebug() << "Loading translation from" << translator.filePath();
