245f04924894a8c0885e11b4ab548009a59d76d6
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-02-21 18:04:16 +0100

    Add a comment to Stub.hh

--- Stub.hh
+++ Stub.hh
@@ -1,3 +1,15 @@
+/*
+ * The goal of this file is to allow compiling the project while not every function is implemented.
+ * This is achieved by printing a warning to the console when an unimplemented function is called.
+ *
+ * To use it, simply use the STUB macro in a header file after a function declaration but before the semicolon.
+ *
+ * Example:
+ * void stubbedFunction(int anArgument) STUB;
+ *
+ * This file should be safe to delete and every reference to it removed at the end of the project.
+ */
+
 #ifndef MYSTICSQUARE_STUB_HH
 #define MYSTICSQUARE_STUB_HH
 
