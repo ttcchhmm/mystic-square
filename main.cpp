6940111cb2745993b7f9fb3b4ad7b19cc6ce2cc6
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-03-03 14:32:47 +0100

    Set application name

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QApplication::setApplicationName("MysticSquare");
+    QApplication::setApplicationDisplayName(QApplication::tr("Mystic Square", "Application display name"));
     QApplication::setWindowIcon(QIcon(":/assets/icon.png"));
 
     qSetMessagePattern(QStringLiteral("[%{type} / %{file}:%{line} / %{function} / %{time yyyy-MM-dd h:mm:ss}]: %{message}"));
